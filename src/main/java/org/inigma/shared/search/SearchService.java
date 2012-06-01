7e1436f34be75d542c7e37b0fcada33e3a8eee7b
Sejal Patel <sejal@inigma.org>
2012-06-01 09:20:51 -0400

    Search supports field specification and sorting.

--- src/main/java/org/inigma/shared/search/SearchService.java
+++ src/main/java/org/inigma/shared/search/SearchService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Field;
+import org.springframework.data.mongodb.core.query.Order;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Sort;
 import org.springframework.stereotype.Component;
 
 import com.mongodb.BasicDBObject;
@@ -47,15 +49,35 @@ public class SearchService {
         if (criteria.getQuery() != null) {
             query = (DBObject) JSON.parse(criteria.getQuery());
         }
+        if (criteria.getFields() != null) {
+            fields = (DBObject) JSON.parse(criteria.getFields());
+        }
+        if (criteria.getSort() != null) {
+            sort = (DBObject) JSON.parse(criteria.getSort());
+        }
 
         Criteria c = convertDbObject(query);
         Query q = Query.query(c);
-        /*
         Field f = q.fields();
         for (String key : fields.keySet()) {
-            // TODO: 0 = hide, 1 = show
+            if (fields.get(key).equals(1)) {
+                f.include(key);
+            } else if (fields.get(key).equals(0)) {
+                f.exclude(key);
+            } else {
+                logger.error("Not sure what field {} is exactly but think it is {}", fields.get(key), fields.get(key)
+                        .getClass());
+            }
         }
-        */
+        Sort s = q.sort();
+        for (String key : sort.keySet()) {
+            Integer direction = (Integer) sort.get(key);
+            if (direction < 0) {
+                s.on(key, Order.DESCENDING);
+            } else if (direction > 0) {
+                s.on(key, Order.ASCENDING);
+            }
+        }
         q.skip(criteria.getPage() * criteria.getRows());
         q.limit(criteria.getRows());
 
