3cbc384d0845c9973899cec426856fab9378f486
Sejal Patel <sejal@inigma.org>
2012-09-08 23:04:44 -0400

    Enhanced error messaging

--- src/main/java/org/inigma/shared/webapp/ErrorPageController.java
+++ src/main/java/org/inigma/shared/webapp/ErrorPageController.java
@@ -1,5 +1,6 @@
 package org.inigma.shared.webapp;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -11,25 +12,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ErrorPageController {
     private static class ErrorPageResponse {
         private int code;
-        private String message;
+        private HttpServletRequest req;
 
-        public ErrorPageResponse(int code, String msg) {
+        public ErrorPageResponse(int code, HttpServletRequest req) {
             this.code = code;
-            this.message = msg;
+            this.req = req;
         }
 
         public int getCode() {
             return code;
         }
 
-        public String getMessage() {
-            return message;
+        public String getContextPath() {
+            return req.getContextPath();
+        }
+
+        public Cookie[] getCookies() {
+            return req.getCookies();
+        }
+
+        public String getMethod() {
+            return req.getMethod();
+        }
+        
+        public String getUri() {
+            return (String) req.getAttribute("javax.servlet.forward.request_uri");
         }
     }
 
     @RequestMapping("/error/page/{code}")
     @ResponseBody
     public ErrorPageResponse handleErrorPage(@PathVariable int code, HttpServletRequest req) {
-        return new ErrorPageResponse(code, req.toString());
+        return new ErrorPageResponse(code, req);
     }
 }
