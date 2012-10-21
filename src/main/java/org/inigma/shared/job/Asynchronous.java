e132cc55422280ff083eec3d4fbb8f0a424d4b9b
Sejal Patel <sejal@inigma.org>
2012-10-21 17:01:16 -0400

    Threadpool Based Asynchronous Method Invoker
    Squashed commit of the following:
    
    commit 92946b4fbda86e7ad24d17c8707ce4f5d4f04ac1
    Author: Sejal Patel <sejal@inigma.org>
    Date:   Sun Oct 21 17:00:37 2012 -0400
    
        Auto-initialize and removed spring hooks.
    
        Wanted this stuff to work in a non-spring injected fashion.
    
    commit aeb1bcb944d9d05edb90575885da5de52f79a53d
    Author: Sejal Patel <sejal@inigma.org>
    Date:   Sun Oct 21 16:41:02 2012 -0400
    
        Asynchronous Method Executor within Threadpool

---
+++ src/main/java/org/inigma/shared/job/Asynchronous.java
@@ -0,0 +1,135 @@
+package org.inigma.shared.job;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.util.ClassUtils;
+
+public class Asynchronous {
+    private static Logger logger = LoggerFactory.getLogger(Asynchronous.class);
+
+    private BlockingDeque<FutureTask<?>> workQueue = new LinkedBlockingDeque<FutureTask<?>>();
+    private ThreadPoolTaskExecutor executor;
+    private long monitorInterval = 10000;
+
+    public Asynchronous() {
+        this(5);
+    }
+
+    public Asynchronous(int workers) {
+        executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(workers + 1);
+        executor.setDaemon(true);
+        initialize();
+    }
+
+    @PreDestroy
+    public void close() {
+        executor.shutdown();
+    }
+
+    public void initialize() {
+        executor.initialize();
+        for (int i = 1; i < executor.getCorePoolSize(); i++) {
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            workQueue.take().run();
+                        } catch (InterruptedException e) {
+                            // ignore
+                        } catch (Throwable e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        }
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    monitor();
+                    try {
+                        Thread.sleep(monitorInterval);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+    }
+
+    public <T> Future<T> invoke(final Object instance, final String method, final Object... args) {
+        FutureTask<T> task = createTask(instance, method, args);
+        try {
+            workQueue.put(task);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return task;
+    }
+
+    public void monitor() {
+        int size = workQueue.size();
+        if (size > 0) {
+            logger.info("Work Queue {}", size);
+        }
+    }
+
+    public void setMonitorInterval(long monitorInterval) {
+        this.monitorInterval = monitorInterval;
+    }
+
+    public void setWorkers(int workers) {
+        executor.setCorePoolSize(workers + 1);
+    }
+
+    protected <T> FutureTask<T> createTask(final Object instance, final String method, final Object... args) {
+        Method actualMethod = null;
+        for (Method m : instance.getClass().getMethods()) {
+            if (method.equals(m.getName())) {
+                Class<?>[] types = m.getParameterTypes();
+                if (args.length == types.length) {
+                    boolean valid = true;
+                    for (int i = 0; i < args.length; i++) {
+                        Class<?> paramType = ClassUtils.resolvePrimitiveIfNecessary(types[i]);
+                        if (!(args[i].getClass().isAssignableFrom(paramType))) {
+                            valid = false;
+                        }
+                    }
+                    if (valid) {
+                        actualMethod = m;
+                        break;
+                    }
+                }
+            }
+        }
+        if (actualMethod == null) {
+            throw new IllegalStateException("Method " + method + " not found with given arguments");
+        }
+
+        final Method realMethod = actualMethod;
+        return new FutureTask<T>(new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return (T) realMethod.invoke(instance, args);
+            }
+        });
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        close();
+        super.finalize();
+    }
+}
