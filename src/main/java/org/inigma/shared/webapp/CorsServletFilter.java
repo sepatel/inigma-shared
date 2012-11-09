0c9c957f3fb02125e86d1e0934998b53ad3ac285
Sejal Patel <sejal@inigma.org>
2012-11-09 18:55:47 -0500

    Improved CORS Support
    Better compliance with CORS standards using the Request options to set
    the Allowed options on the response. This gives more precision rather
    then blanket coverage attempts.

--- src/main/java/org/inigma/shared/webapp/CorsServletFilter.java
+++ src/main/java/org/inigma/shared/webapp/CorsServletFilter.java
@@ -8,30 +8,44 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * This servlet filter adds the headers needed to allow CORS (Cross Origin Resource Sharing) compliant browsers to be
  * able and make direct ajax calls without the need for a proxy.
+ * 
  * @author <a href="mailto:sejal@inigma.org">Sejal Patel</a>
  */
 public class CorsServletFilter implements Filter {
-    private String accessAllowOrigin;
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        accessAllowOrigin = filterConfig.getInitParameter("allow-origin");
-        if (accessAllowOrigin == null) {
-            accessAllowOrigin = "*";
-        }
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain f) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
-        response.setHeader("Access-Control-Allow-Origin", accessAllowOrigin);
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE"); // full rest support
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with"); // jquery 1.5 support
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        }
+
+        String origin = request.getHeader("Origin");
+        if (origin == null) {
+            origin = "*";
+        }
+        response.setHeader("Access-Control-Allow-Origin", origin);
+
+        String headers = request.getHeader("Access-Control-Request-Headers");
+        if (headers != null) {
+            response.setHeader("Access-Control-Allow-Headers", headers);
+        }
+
+        String method = request.getHeader("Access-Control-Request-Method");
+        if (method != null) {
+            response.setHeader("Access-Control-Allow-Methods", method); // full rest support
+        }
+        response.setHeader("Access-Control-Max-Age", "10"); // 10 seconds
 
         f.doFilter(req, resp);
     }
