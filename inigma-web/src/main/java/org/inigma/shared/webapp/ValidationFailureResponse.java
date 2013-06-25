3059b9612d36eb59ac48f16565598715db2bce3a
Sejal Patel <sejal@inigma.org>
2013-06-25 15:20:30 -0400

    Made Validation Failure Response publicly visible.
    Issue cropped up that any controller wrapped with CGI Lib (such as what
    happens to everything protected by spring security) was erroring out on
    a validation failure when protected because it was not visible to the
    wrapped class and thus could not be proxied through correctly.

--- inigma-web/src/main/java/org/inigma/shared/webapp/ValidationFailureResponse.java
+++ inigma-web/src/main/java/org/inigma/shared/webapp/ValidationFailureResponse.java
@@ -3,7 +3,7 @@ package org.inigma.shared.webapp;
 import java.util.ArrayList;
 import java.util.Collection;
 
-class ValidationFailureResponse {
+public class ValidationFailureResponse {
     private static class FieldErrorResponse extends ObjectErrorResponse {
         private String field;
 
