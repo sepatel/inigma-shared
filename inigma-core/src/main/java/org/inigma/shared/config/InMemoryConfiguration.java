6033caf55468384cf04e913e8a246e51fb93193e
Sejal Patel <sejal@inigma.org>
2013-09-30 14:55:10 -0400

    Added ability to spring configure in-memory config

--- inigma-core/src/main/java/org/inigma/shared/config/InMemoryConfiguration.java
+++ inigma-core/src/main/java/org/inigma/shared/config/InMemoryConfiguration.java
@@ -1,8 +1,22 @@
 package org.inigma.shared.config;
 
+import java.util.Map;
+
 public class InMemoryConfiguration extends AbstractConfiguration {
+    public InMemoryConfiguration() {
+    }
+
+    public InMemoryConfiguration(Map<String, Object> values) {
+        setValues(values);
+    }
+
+    public void setValues(Map<String, Object> values) {
+        super.reload(values);
+    }
+
     @Override
-    protected void setValue(String key, Object value) {
+    protected <T> T getValue(String key, Class<T> type) {
+        return null;
     }
 
     @Override
@@ -10,7 +24,6 @@ public class InMemoryConfiguration extends AbstractConfiguration {
     }
 
     @Override
-    protected <T> T getValue(String key, Class<T> type) {
-        return null;
+    protected void setValue(String key, Object value) {
     }
 }
