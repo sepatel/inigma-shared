2cc5b5d5f863149f1e409b1fc605c01a1c531823
Grant Overby <grant.overby@stratixcorp.com>
2013-10-28 13:03:49 -0400

    method to convert snake case to camel case

--- inigma-core/src/test/java/org/inigma/shared/tools/StringUtilTest.java
+++ inigma-core/src/test/java/org/inigma/shared/tools/StringUtilTest.java
@@ -1,4 +1,23 @@
 package org.inigma.shared.tools;
 
+import org.junit.Test;
+
+import static org.inigma.shared.tools.StringUtil.*;
+import static org.junit.Assert.*;
+
 public class StringUtilTest {
+    @Test
+    public void snakeToCamelTest() {
+        assertEquals("one","one", snakeToCamel("one"));
+        assertEquals("one_two","oneTwo", snakeToCamel("one_two"));
+        assertEquals("_two","Two", snakeToCamel("_two"));
+        assertEquals("null",null, snakeToCamel(null));
+        assertEquals("empty","", snakeToCamel(""));
+        assertEquals("oNe_tWo","oneTwo", snakeToCamel("oNe_tWo"));
+        assertEquals("one_two_three_four","oneTwoThreeFour", snakeToCamel("one_two_three_four"));
+        assertEquals("one__two","oneTwo", snakeToCamel("one__two"));
+        assertEquals("one_two_","oneTwo", snakeToCamel("one_two_"));
+    }
+
+
 }
