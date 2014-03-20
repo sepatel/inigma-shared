d2a3db3cf1da58d434b1c0d599331aa9190186e2
Sejal Patel <sejal@inigma.org>
2014-03-20 15:03:09 -0400

    Fix to support oracle java

--- inigma-core/src/main/java/org/inigma/shared/crypto/RsaKeyPair.java
+++ inigma-core/src/main/java/org/inigma/shared/crypto/RsaKeyPair.java
@@ -1,25 +1,54 @@
 package org.inigma.shared.crypto;
 
+import java.math.BigInteger;
 import java.security.spec.RSAPrivateKeySpec;
 import java.security.spec.RSAPublicKeySpec;
 
 public class RsaKeyPair {
-    private RSAPrivateKeySpec privateKey;
-    private RSAPublicKeySpec publicKey;
-
-    public RSAPrivateKeySpec getPrivateKey() {
-        return privateKey;
+    private static class PrivateKeySpec {
+        private BigInteger modulus;
+        private BigInteger privateExponent;
     }
 
-    public RSAPublicKeySpec getPublicKey() {
-        return publicKey;
+    private static class PublicKeySpec {
+        private BigInteger modulus;
+        private BigInteger publicExponent;
+    }
+
+    private PrivateKeySpec privateKey;
+    private PublicKeySpec publicKey;
+
+    public RSAPrivateKeySpec getPrivateKey() {
+        if (privateKey == null) {
+            return null;
+        }
+        return new RSAPrivateKeySpec(privateKey.modulus, privateKey.privateExponent);
     }
 
     public void setPrivateKey(RSAPrivateKeySpec privateKey) {
-        this.privateKey = privateKey;
+        if (privateKey == null) {
+            this.privateKey = null;
+        } else {
+            this.privateKey = new PrivateKeySpec();
+            this.privateKey.modulus = privateKey.getModulus();
+            this.privateKey.privateExponent = privateKey.getPrivateExponent();
+        }
+    }
+
+    public RSAPublicKeySpec getPublicKey() {
+        if (publicKey == null) {
+            return null;
+        }
+        return new RSAPublicKeySpec(publicKey.modulus, publicKey.publicExponent);
     }
 
     public void setPublicKey(RSAPublicKeySpec publicKey) {
-        this.publicKey = publicKey;
+        if (publicKey == null) {
+            this.publicKey = null;
+        } else {
+            this.publicKey = new PublicKeySpec();
+            this.publicKey.modulus = publicKey.getModulus();
+            this.publicKey.publicExponent = publicKey.getPublicExponent();
+        }
     }
 }
