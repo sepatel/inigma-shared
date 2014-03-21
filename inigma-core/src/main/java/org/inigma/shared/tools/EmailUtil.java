bbcc4cf95eab088c140e9c3ad0e8284fe9549cc2
Sejal Patel <sejal@inigma.org>
2014-03-20 23:34:03 -0400

    Reformatting

--- inigma-core/src/main/java/org/inigma/shared/tools/EmailUtil.java
+++ inigma-core/src/main/java/org/inigma/shared/tools/EmailUtil.java
@@ -8,8 +8,7 @@ import java.util.regex.Pattern;
  */
 public abstract class EmailUtil {
     private static Pattern EMAIL_PATTERN = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@" +
-            "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+(?:[a-z]{2}|com|org|net|edu|gov|mil|" +
-            "biz|info|mobi|name|aero|asia|jobs|museum)\\b");
+            "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+(?:[a-z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\\b");
 
     public static String fixEmail(String email) {
         if (email == null || "".equals(email.trim())) {
